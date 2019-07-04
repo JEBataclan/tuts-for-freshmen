e255e772c98e5630a19a8930459e1cd56af322f7
Jan Bataclan <jebataclan03@gmail.com>
2019-07-04 20:54:21 +0800

    Update operators.c

--- operators.c
+++ operators.c
@@ -8,19 +8,24 @@ int main() {
     int diff = num1 - num2;
     int prod = num1 * num2;
     int quot = num2 / num1; //you may use float for decimal
-                            //but num1 and num2 must be float also
+                            //but num1 and num2 must be float also.
+    int rem = 5 % 3;        //modulo operator, gets the remainder.
+    
+    
     printf("%d\n", sum);
     printf("%d\n", diff);
     printf("%d\n", prod);
-    printf("%d", quot);
+    printf("%d\n", quot);
+    printf("%d", rem);
 }
 
 /*
-possible to produce the same output w/o using variables sum, diff, prod, quot
+possible to produce the same output w/o using variables sum, diff, prod, quot, and rem
 by changing the variables in printf to operations instead:
 
 printf("%d\n", num1 + num2);
 printf("%d\n", num1 - num2);
 printf("%d\n", num1 * num2);
 printf("%d\n", num1 / num2);
+printf("%d", 5 % 3);
 */
